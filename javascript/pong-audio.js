392bd8bc68d6ec66a1eea0d61d83765a5af856b6
mhmccasland <mhmccasland@gmail.com>
2025-01-02 21:33:58 -0600

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -46,7 +46,7 @@ export var soundArray = []; //list of sounds loaded
 export var wallSound = new soundFile("silence.mp3"); //load sound
 soundArray.push(wallSound); //add sound to list of sounds
 
-export var paddleSound = new soundFile("tear vessel.aup3");
+export var paddleSound = new soundFile("tear vessel.mp3");
 soundArray.push(paddleSound);
 
 export var scoreSound = new soundFile("item get.mp3");
